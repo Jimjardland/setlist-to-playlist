d6611946b0b454742a700d3a20289519302cb78a
Jimmy Jardland <jimmy@jardland.se>
2017-09-16 19:14:10 +0200

    Dockerized the application and some fixes in the front

--- index.js
+++ index.js
@@ -8,23 +8,16 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api', router)
-app.use('/', express.static('client'));
+app.use('/', express.static('client'))
 app.get('/', async (req, res) => res.redirect(await auth.getLoginUrl()))
 app.get('/callback', (req, res) => res.sendFile(path.resolve('index.html')))
 
 router.post('/create-playlist', async (req, res, next) => {
   createPlaylist(req.body.artist, req.body.token)
     .then(message => res.send({ message }))
-    .catch(error => {
-      error.statusCode = 400
-      res.send({ message: error.message })
-    })
-  
+    .catch(error => res.status(400).send({ message: error.message }))
 })
 
 
